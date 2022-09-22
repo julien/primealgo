81cd01c6b004502268dd10ffc210556180e58668
Julien Castelain <julien@users.noreply.github.com>
2022-09-22 21:30:53 +0200

    feat: add (doubly) linked list example

---
+++ linked_list.go
@@ -0,0 +1,173 @@
+package main
+
+import "errors"
+
+type Node[T comparable] struct {
+	value      T
+	prev, next *Node[T]
+}
+
+var insertionError = errors.New("can't insert at specified index")
+
+type LinkedList[T comparable] struct {
+	length     int
+	head, tail *Node[T]
+}
+
+func NewLinkedList[T comparable]() *LinkedList[T] {
+	return &LinkedList[T]{length: 0, head: nil, tail: nil}
+}
+
+func (l LinkedList[T]) Length() int {
+	return l.length
+}
+
+func (l *LinkedList[T]) InsertAt(item T, idx int) error {
+	if idx > l.length {
+		return insertionError
+	}
+
+	if idx == l.length {
+		l.Append(item)
+		return nil
+	} else if idx == 0 {
+		l.Prepend(item)
+		return nil
+	}
+
+	l.length++
+	curr := l.head
+	for i := 0; curr != nil && i < idx; i++ {
+		curr = curr.next
+	}
+
+	node := &Node[T]{value: item}
+	node.next = curr
+	prev := curr.prev
+	node.prev = prev
+	curr.prev = node
+	prev.next = node
+
+	return nil
+}
+
+func (l *LinkedList[T]) Remove(item T) (result T) {
+	curr := l.head
+	for i := 0; curr != nil && i < l.length; i++ {
+		if curr.value == item {
+			break
+		}
+		curr = curr.next
+	}
+
+	if curr == nil {
+		return
+	}
+
+	l.length--
+
+	if l.length == 0 {
+		out := l.head.value
+		l.head = nil
+		l.tail = nil
+		return out
+	}
+
+	if curr.prev != nil {
+		curr.prev.next = curr.next
+	}
+	if curr.next != nil {
+		curr.next.prev = curr.prev
+	}
+	if curr == l.head {
+		l.head = curr.next
+	}
+	if curr == l.tail {
+		l.tail = curr.prev
+	}
+
+	curr.prev = nil
+	curr.next = nil
+
+	result = curr.value
+	return result
+}
+
+func (l *LinkedList[T]) RemoveAt(idx int) T {
+	var result T
+	curr := l.head
+	for i := 0; curr != nil && i < idx; i++ {
+		curr = curr.next
+	}
+
+	if curr == nil {
+		return result
+	}
+
+	l.length--
+
+	if l.length == 0 {
+		out := l.head.value
+		l.head = nil
+		l.tail = nil
+		return out
+	}
+
+	if curr.prev != nil {
+		curr.prev.next = curr.next
+	}
+	if curr.next != nil {
+		curr.next.prev = curr.prev
+	}
+	if curr == l.head {
+		l.head = curr.next
+	}
+	if curr == l.tail {
+		l.tail = curr.prev
+	}
+
+	curr.prev = nil
+	curr.next = nil
+
+	result = curr.value
+	return result
+}
+
+func (l *LinkedList[T]) Append(item T) {
+	l.length++
+
+	node := &Node[T]{value: item}
+
+	if l.tail == nil {
+		l.head = node
+		l.tail = node
+		return
+	}
+
+	node.prev = l.tail
+	l.tail.next = node
+	l.tail = node
+}
+
+func (l *LinkedList[T]) Prepend(item T) {
+	node := &Node[T]{value: item}
+
+	l.length++
+	if l.head == nil {
+		l.head = node
+		l.tail = node
+		return
+	}
+
+	node.next = l.head
+	l.head.prev = node
+	l.head = node
+}
+
+func (l LinkedList[T]) Get(idx int) T {
+	curr := l.head
+	for i := 0; curr != nil && i < idx; i++ {
+		curr = curr.next
+	}
+	return curr.value
+}
---
+++ linked_list_test.go
@@ -0,0 +1,73 @@
+package main
+
+import "testing"
+
+func TestLinkedList(t *testing.T) {
+	list := NewLinkedList[int]()
+	list.Append(5)
+	list.Append(7)
+	list.Append(9)
+
+	if v := list.Get(2); v != 9 {
+		t.Errorf("got %v, want 9", v)
+	}
+	if v := list.RemoveAt(1); v != 7 {
+		t.Errorf("got %v, want 7", v)
+	}
+	if v := list.Length(); v != 2 {
+		t.Errorf("got %v, want 2", v)
+	}
+
+	list.Append(11)
+	if v := list.RemoveAt(1); v != 9 {
+		t.Errorf("got %v, want 9", v)
+	}
+	if v := list.Remove(9); v != 0 {
+		t.Errorf("got %v, want 0", v)
+	}
+	if v := list.RemoveAt(0); v != 5 {
+		t.Errorf("got %v, want 5", v)
+	}
+	if v := list.RemoveAt(0); v != 11 {
+		t.Errorf("got %v, want 11", v)
+	}
+	if v := list.Length(); v != 0 {
+		t.Errorf("got %v, want 0", v)
+	}
+
+	list.Prepend(5)
+	list.Prepend(7)
+	list.Prepend(9)
+
+	if v := list.Get(2); v != 5 {
+		t.Errorf("got %v, want 5", v)
+	}
+	if v := list.Get(0); v != 9 {
+		t.Errorf("got %v, want 9", v)
+	}
+	if v := list.Remove(9); v != 9 {
+		t.Errorf("got %v, want 9", v)
+	}
+	if v := list.Length(); v != 2 {
+		t.Errorf("got %v, want 2", v)
+	}
+	if v := list.Get(0); v != 7 {
+		t.Errorf("got %v, want 7", v)
+	}
+
+	list.InsertAt(3, 0)
+	if v := list.Get(0); v != 3 {
+		t.Errorf("got %v, want 3", v)
+	}
+	if v := list.Length(); v != 3 {
+		t.Errorf("got %v, want 3", v)
+	}
+
+	list.InsertAt(4, 1)
+	if v := list.Get(1); v != 4 {
+		t.Errorf("got %v, want 4", v)
+	}
+	if v := list.Length(); v != 4 {
+		t.Errorf("got %v, want 4", v)
+	}
+}
